0894c303ec09b9c46af1fe6270bf696d0059391b
Delaney Burke <delaney@vidtreon.com>
2022-06-15 18:04:44 +0300

    Add seed data
    Generate 10 members

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Member.create first_name: Faker::Name.first_name,
+                surname: Faker::Name.last_name,
+                email_address: Faker::Internet.email,
+                date_of_birth: Faker::Date.between(from: '1970-01-01', to: '2010-01-31')
+end
