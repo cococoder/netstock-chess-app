5ae5d5c6a7145b362d21706b5f4476dfafca1a08
Delaney Burke <delaney@vidtreon.com>
2022-06-15 17:31:33 +0300

    Generate: Leaderboard
    rails g controller Leaderboard index and set index as route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'leaderboard/index'
   resources :chess_games
   resources :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "leaderboard#index"
 end
