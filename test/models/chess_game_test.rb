9f7e2870b0da3545313ee039b56737be84a34232
Delaney Burke <delaney@vidtreon.com>
2022-06-16 06:25:08 +0300

    Test: lower ranked (10) member beating higher (1)
    result: failing
    
    next:
    
    - move the higher member down 1
    - move the lower member up 5 places ( half way between where they currently are)

--- test/models/chess_game_test.rb
+++ test/models/chess_game_test.rb
@@ -27,4 +27,18 @@ class ChessGameTest < ActiveSupport::TestCase
     assert higher_ranked_player.rank == 1
     assert lower_ranked_player.rank == 2
   end
+
+  test "lower ranked player winning " do
+
+    higher_ranked_player = members(:member_1)
+    lower_ranked_player = members(:member_10)
+
+    ChessGame.create black_player: higher_ranked_player,
+                     white_player: lower_ranked_player,
+                     draw:false,
+                     winner: lower_ranked_player
+
+    assert_equal 2, higher_ranked_player.rank, "high ranked player did not move down 1"
+    assert_equal 5,lower_ranked_player.rank, "lower ranked player did not move up 5"
+  end
 end
